27a7a4d7b32c881d6c53e2cad095871cc7cc53e1
mnovati <michael@novati.ca>
2018-04-15 01:27:21 +0000

    caching in db layer

--- classes/Viewer.js
+++ classes/Viewer.js
@@ -2,6 +2,7 @@ class Viewer {
 
   constructor(viewer_id) {
     this.id = viewer_id;
+    this.cache = {};
   }
 
   getID() {
@@ -14,6 +15,22 @@ class Viewer {
 
   isWriteAll() {
     return false;
+  }
+
+  existsCache(key) {
+    return key in this.cache;
+  }
+
+  fromCache(key) {
+    return this.cache[key];
+  }
+
+  setCache(key, value) {
+    this.cache[key] = value;
+  }
+
+  deleteCache(key) {
+    delete this.cache[key];
   }
 }
 
