c4dbacfb820427c9ee15723530a90aa4a9141429
mnovati <michael@novati.ca>
2018-09-05 17:13:31 +0000

    pagination

--- utils/GraphQL.js
+++ utils/GraphQL.js
@@ -10,11 +10,26 @@ async function parseSet(ng, DB, viewer, object, nodes) {
   await Promise.all(nodes.selections.map(async node => {
     if (object) {
       var to_id = null;
-      (node.arguments || []).forEach(arg => {
+      var count = null;
+      var offset = null;
+      var after = null;
+      await Promise.all((node.arguments || []).map(async arg => {
         if (arg.name.value === 'to_id') {
           to_id = arg.value.value;
+        } else if (arg.name.value === 'first') {
+          count = parseInt(arg.value.value);
+        } else if (arg.name.value === 'offset') {
+          offset = parseInt(arg.value.value);
+          if (after !== null) {
+            after = null;
+          }
+        } else if (arg.name.value === 'after') {
+          after = arg.value.value;
+          if (offset !== null) {
+            offset = null;
+          }
         }
-      });
+      }));
       var ids_to_fetch = {};
       var edge_type = ng.CONSTANTS.getEdgeTypeFromName(object.getType(), node.name.value);
       if (to_id) {
@@ -23,10 +38,20 @@ async function parseSet(ng, DB, viewer, object, nodes) {
       } else {
         result = await DB.getEdge(viewer, object.getID(), edge_type);
       }
-      (result || []).filter(Boolean).forEach((edge) => {
-        edges.push(edge);
-        ids_to_fetch[edge.getToID()] = true;
-      });
+      result = (result || []).filter(Boolean);
+
+      // pagination
+      count = count === null ? result.length : count;
+      var add = after === null && offset === null;
+      for (var ii = 0; ii < result.length && count > 0; ii++) {
+        add = add || (offset !== null && offset === ii);
+        if (add) {
+          edges.push(edge);
+          ids_to_fetch[result[ii].getToID()];
+          count--;
+        }
+        add = add || (after !== null && result[ii].getToID() === after);
+      }
       await Promise.all(Object.keys(ids_to_fetch).map(async object_id => {
         var object = await DB.getObject(viewer, object_id);
         objects[object_id] = object;
@@ -34,22 +59,36 @@ async function parseSet(ng, DB, viewer, object, nodes) {
     } else {
       var type = ng.CONSTANTS.getObjectTypeFromName(node.name.value);
       var object_ids = [];
-      if ((node.arguments || []).length > 0) {
-        await Promise.all(node.arguments.map(async arg => {
-          if (arg.name.value === 'id') {
-            object_ids.push(arg.value.value);
-          } else if (arg.name.value === 'ids') {
-            arg.value.values.forEach(id => object_ids.push(id.value));
-          } else if (arg.name.value === 'point') {
-            var [lat, lng, distance] = arg.value.values;
-            var matches = await DB.lookupGeoIndex({lat: lat.value, lng: lng.value}, [type], (distance.value || 1) * 1.6 * 1000);
-            (matches || []).forEach(id => object_ids.push(id));
-          } else {
-            var matches = await DB.lookupIndex(type, arg.name.value, arg.value.value);
-            (matches || []).forEach(id => object_ids.push(id));
+      var count = null;
+      var offset = null;
+      var after = null;
+      await Promise.all((node.arguments || []).map(async arg => {
+        if (arg.name.value === 'id') {
+          object_ids.push(arg.value.value);
+        } else if (arg.name.value === 'ids') {
+          arg.value.values.forEach(id => object_ids.push(id.value));
+        } else if (arg.name.value === 'point') {
+          var [lat, lng, distance] = arg.value.values;
+          var matches = await DB.lookupGeoIndex({lat: lat.value, lng: lng.value}, [type], (distance.value || 1) * 1.6 * 1000);
+          (matches || []).forEach(id => object_ids.push(id));
+        } else if (arg.name.value === 'first') {
+          count = parseInt(arg.value.value);
+        } else if (arg.name.value === 'offset') {
+          offset = parseInt(arg.value.value);
+          if (after !== null) {
+            after = null;
           }
-        }));
-      } else {
+        } else if (arg.name.value === 'after') {
+          after = arg.value.value;
+          if (offset !== null) {
+            offset = null;
+          }
+        } else {
+          var matches = await DB.lookupIndex(type, arg.name.value, arg.value.value);
+          (matches || []).forEach(id => object_ids.push(id));
+        }
+      }));
+      if (object_ids.length === 0) {
         var config = ng.CONSTANTS.getObject(type);
         if (!config.root_id) {
           NovaError.throwError('Cannot fetch all objects for given type');
@@ -57,13 +96,28 @@ async function parseSet(ng, DB, viewer, object, nodes) {
         var edge = await DB.getEdge(viewer, config.root_id, ng.CONSTANTS.ROOT_EDGE);
         edge.forEach(e => object_ids.push(e.getToID()));
       }
+      var fetched = [];
       await Promise.all(object_ids.map(async object_id => {
         var object = await DB.getObject(viewer, object_id);
         if (object && (object.getType() !== type)) {
           NovaError.throwError('Object type does not match requested type');
         }
-        objects[object_id] = object;
+        if (object) {
+          fetched.push(object);
+        }
       }));
+
+      // pagination
+      count = count === null ? fetched.length : count;
+      var add = after === null && offset === null;
+      for (var ii = 0; ii < fetched.length && count > 0; ii++) {
+        add = add || (offset !== null && offset === ii);
+        if (add) {
+          objects[fetched[ii].getID()] = fetched[ii];
+          count--;
+        }
+        add = add || (after !== null && fetched[ii].getID() === after);
+      }
     }
     await Promise.all(Object.keys(objects).map(async object_id => {
       if (!objects[object_id]) {
