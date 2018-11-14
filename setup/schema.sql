3027019efa13cea3a40fef861f243cdee0fcf89d
mnovati <michael@novati.ca>
2018-11-14 00:40:35 +0000

    defer index

--- setup/schema.sql
+++ setup/schema.sql
@@ -29,18 +29,7 @@ RETURN
 //
 DELIMITER ;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `novagraph` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `novagraph`;
 
 USE `novagraph`;
 
@@ -49,8 +38,6 @@ USE `novagraph`;
 --
 
 DROP TABLE IF EXISTS `edges`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `edges` (
   `from_id` binary(16) DEFAULT NULL,
   `type` smallint(5) unsigned DEFAULT NULL,
@@ -61,15 +48,12 @@ CREATE TABLE `edges` (
   `status` smallint(6) DEFAULT NULL,
   KEY `fromidtype` (`from_id`,`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `objects`
 --
 
 DROP TABLE IF EXISTS `objects`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `objects` (
   `id` binary(16) NOT NULL,
   `type` smallint(5) unsigned DEFAULT NULL,
@@ -79,39 +63,24 @@ CREATE TABLE `objects` (
   `status` smallint(6) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 --
 -- Table structure for table `indices`
 --
 
 DROP TABLE IF EXISTS `indices`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `indices` (
   `key` binary(20) NOT NULL,
   `value` binary(16) NOT NULL,
   KEY `keyvalue` (`key`,`value`),
   INDEX `hashlookup` (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `geo_indices`
 --
 
 DROP TABLE IF EXISTS `geoindices`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `geoindices` (
   `id` binary(26) NOT NULL,
   `type` smallint(5) unsigned DEFAULT NULL,
@@ -119,15 +88,12 @@ CREATE TABLE `geoindices` (
   PRIMARY KEY (`id`),
   SPATIAL KEY `shape` (`shape`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `fts_indices`
 --
 
 DROP TABLE IF EXISTS `ftsindices`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ftsindices` (
   `id` binary(26) NOT NULL,
   `type` smallint(5) unsigned NOT NULL,
@@ -136,4 +102,17 @@ CREATE TABLE `ftsindices` (
   INDEX (`type`),
   FULLTEXT (`data`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `deferindices`
+--
+
+DROP TABLE IF EXISTS `deferindices`;
+CREATE TABLE `deferindices` (
+  `id` binary(26) NOT NULL,
+  `type` smallint(5) unsigned NOT NULL,
+  `defer_time` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX (`type`, `defer_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
