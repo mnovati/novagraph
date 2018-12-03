837c0befbb626f6fe17486641eb772d8e62650cd
mnovati <michael@novati.ca>
2018-12-03 03:38:29 +0000

    fix

--- utils/GraphQL.js
+++ utils/GraphQL.js
@@ -368,7 +368,7 @@ async function parseMutationSet(ng, DB, viewer, object, nodes) {
       if (!objects[object_id]) {
         return;
       }
-      var [more_objects, more_edges] = await parseSet(ng, DB, viewer, objects[object_id], node.selectionSet);
+      var [more_objects, more_edges] = await parseMutationSet(ng, DB, viewer, objects[object_id], node.selectionSet);
       Object.keys(more_objects).map(i => objects[i] = more_objects[i]);
       more_edges.forEach(e => edges.push(e));
     }));
