b0511f3c7455cb036d1f024d55952eee317fb459
mnovati <michael@novati.ca>
2019-03-13 18:50:52 +0000

    improve

--- classes/GAllowViewerEdgeRule.js
+++ classes/GAllowViewerEdgeRule.js
@@ -2,12 +2,28 @@ const GRule = require('./GRule.js');
 
 class GAllowViewerEdgeRule extends GRule {
 
+  constructor(type) {
+    super();
+    if (type !== 'source' && type !== 'dest' && type !== 'either') {
+      type = either;
+    }
+    this.type = type;
+  }
+
   async can(edge) {
     if (edge.getViewer().isLoggedOut()) {
       return this.skip();
     }
     var viewer_id = edge.getViewer().getID();
-    return (viewer_id === edge.getToID() || viewer_id === edge.getFromID()) ? this.pass() : this.skip();
+    switch (this.type) {
+      case 'source':
+        return viewer_id === edge.getFromID()? this.pass() : this.skip();
+      case 'dest':
+        return viewer_id === edge.getToID()? this.pass() : this.skip();
+      case 'either':
+      default:
+        return (viewer_id === edge.getToID() || viewer_id === edge.getFromID()) ? this.pass() : this.skip();
+    }
   }
 }
 
