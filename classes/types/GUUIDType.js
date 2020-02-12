39a595e8c2aec1841c91a3c88d7d413d7c23e6f3
mnovati <michael@novati.ca>
2020-02-12 23:22:01 +0000

    .

--- classes/types/GUUIDType.js
+++ classes/types/GUUIDType.js
@@ -13,11 +13,11 @@ class GUUIDType extends GType {
       return false;
     }
     if (this.types.length > 0) {
-      const DB = require('../../lib/db.js');
-      var object = await DB.getObject(viewer.getReadAllViewer(), value);
-      if (!object || !this.types.includes(object.getType())) {
-        return false;
-      }
+      //const DB = require('../../lib/db.js');
+      //var object = await DB.getObject(viewer.getReadAllViewer(), value);
+      //if (!object || !this.types.includes(object.getType())) {
+      //  return false;
+      //}
     }
     return true;
   }
