8130d06cfb14e200d0ac6c17b40bd364a632e306
mnovati <michael@novati.ca>
2020-03-14 18:13:01 +0000

    .

--- classes/GAllowViewerEdgeObjectRule.js
+++ classes/GAllowViewerEdgeObjectRule.js
@@ -4,13 +4,14 @@ const NError = require('../lib/error.js');
 
 class GAllowViewerEdgeObjectRule extends GRule {
 
-  constructor(edges, type) {
+  constructor(edges, type, field_id) {
     super();
     this.edges = edges;
     if (type !== 'source' && type !== 'dest') {
       type = 'dest';
     }
     this.type = type;
+    this.field_id = field_id || null;
   }
 
   async can(object) {
@@ -21,12 +22,13 @@ class GAllowViewerEdgeObjectRule extends GRule {
     if (!Array.isArray(this.edges)) {
       throw NError.normal('Invalid edges provided to edge-based privacy rule');
     }
+    var target_id = this.field_id ? object.object.data[field_id] : object.getID();
     var result = await Promise.all(this.edges.map(async (e) => {
       var edge = await DB.getSingleEdge(
         new ReadAllViewer(0),
-        this.type === 'dest' ? object.getID() : object.getViewer().getID(),
+        this.type === 'dest' ? target_id : object.getViewer().getID(),
         e,
-        this.type === 'dest' ? object.getViewer().getID() : object.getID(),
+        this.type === 'dest' ? object.getViewer().getID() : target_id,
       );
       return !!edge;
     }));
